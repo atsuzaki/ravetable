e41a70e94354683981cd51f34a6c692b613c17af
Katherine Philip <kurerunoshikaku@yahoo.com>
2021-05-27 17:39:25 -0700

    Playing back properly

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -8,32 +8,39 @@
 
 //use rodio::{Decoder, OutputStream, source::Source};
 use hound;
-use log::{info, warn};
+use log::{info, warn, error};
 use std::thread;
 
 use std::fs::File;
 use std::io::{BufReader, Cursor};
 use tuix::Entity;
 use cpal::traits::{HostTrait, DeviceTrait, StreamTrait};
+use hound::WavSpec;
+use cpal::SampleRate;
+use samplerate::{Samplerate, ConverterType};
 
 mod utils;
 mod playback;
 mod gui;
 
+pub struct InputWav {
+	samples: Option<Vec<f32>>,
+	spec: WavSpec,
+	file_path: String,
+}
+
 fn main() -> Result<(), anyhow::Error> {
 	init_logger();
 
-	//// Get a output stream handle to the default physical sound device
-	//let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-	//let sink = rodio::Sink::try_new(&stream_handle).unwrap();
-	//
-	//// Load a sound from a file, using a path relative to Cargo.toml
-	//let file = BufReader::new(File::open("test_wavs/CantinaBand.wav").unwrap());
+	let test_wav = "test_wavs/CantinaBand44100.wav".to_string();
 
-	let mut reader = hound::WavReader::open("test_wavs/CantinaBand.wav").unwrap();
+	let mut reader = hound::WavReader::open(&test_wav).unwrap();
+	let input_wav_spec = reader.spec();
 
-	let mut samples = reader.samples::<f32>();
-	let fsamples: Vec<f32> = samples.map(|f| f.unwrap()).collect();
+
+	let mut samples = reader.into_samples::<f32>();
+	let mut fsamples: Vec<f32> = samples.map(|f| f.unwrap()).collect();
+	fsamples.reverse();
 
 	//hound done, we have f32 samples now
 	//start cpal playback and init and shit
@@ -46,36 +53,57 @@ fn main() -> Result<(), anyhow::Error> {
 	let config = device.default_output_config().unwrap();
 	println!("Default output config: {:?}", config);
 
+	if input_wav_spec.sample_rate != config.sample_rate().0	{
+		println!("Converting sample");
+		// Instanciate a new converter.
+		let mut sample_rate_converter = Samplerate::new(
+			ConverterType::SincBestQuality,
+			input_wav_spec.sample_rate,
+			config.sample_rate().0,
+			input_wav_spec.channels as usize)
+			.unwrap();
+
+
+		// Resample the input from input sample rate to output sample rate
+		fsamples = sample_rate_converter.process_last(&fsamples).unwrap();
+	}
+
+	let mut input_wav = InputWav {
+		samples: Some(fsamples),
+		spec: input_wav_spec,
+		file_path: test_wav,
+	};
+
 	match config.sample_format() {
-		cpal::SampleFormat::F32 => run::<f32>(&device, &config.into(), fsamples),
-		cpal::SampleFormat::I16 => run::<i16>(&device, &config.into(), fsamples),
-		cpal::SampleFormat::U16 => run::<u16>(&device, &config.into(), fsamples),
+		cpal::SampleFormat::F32 => run::<f32>(&device, &config.into(), input_wav),
+		cpal::SampleFormat::I16 => run::<i16>(&device, &config.into(), input_wav),
+		cpal::SampleFormat::U16 => run::<u16>(&device, &config.into(), input_wav),
 	};
 
 	Ok(())
 }
 
-pub fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig, mut samples: Vec<f32>) -> Result<(), anyhow::Error>
+pub fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig, mut input_wav: InputWav) -> Result<(), anyhow::Error>
 	where
 		T: cpal::Sample,
 {
-	let sample_rate = config.sample_rate.0 as f32;
-	let channels = config.channels as usize;
+	let output_sample_rate = config.sample_rate.0 as f32;
+	let output_channels = config.channels as usize;
+	let input_channels = input_wav.spec.channels;
+	let mut samples = input_wav.samples.take().unwrap(); //input_wav's job is basically done now
 
-	// Produce a sinusoid of maximum amplitude.
-	let mut sample_clock = 0f32;
 	let mut next_value = move || {
-		//sample_clock = (sample_clock + 1.0) % sample_rate;
-		//(sample_clock * 440.0 * 2.0 * std::f32::consts::PI / sample_rate).sin()
 		samples.pop().unwrap()
 	};
 
 	let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
 
+	println!("Channels for output: {}", output_channels);
+
 	let stream = device.build_output_stream(
 		config,
 		move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
-			write_data(data, channels, &mut next_value)
+			write_data(data, output_channels, input_channels, &mut next_value)
 		},
 		err_fn,
 	)?;
@@ -86,14 +114,24 @@ pub fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig, mut samples: V
 	Ok(())
 }
 
-fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
+fn write_data<T>(output: &mut [T], output_channels: usize, input_channels: u16, next_sample: &mut dyn FnMut() -> f32)
 	where
 		T: cpal::Sample,
 {
-	for frame in output.chunks_mut(channels) {
-		let value: T = cpal::Sample::from::<f32>(&next_sample());
-		for sample in frame.iter_mut() {
-			*sample = value;
+	for frame in output.chunks_mut(output_channels) {
+		match input_channels {
+			1 => {
+				let value: T = cpal::Sample::from::<f32>(&next_sample());
+				for sample in frame.iter_mut() {
+					*sample = value;
+				}
+			}
+			2 => {
+				for sample in frame.iter_mut() {
+					*sample = cpal::Sample::from::<f32>(&next_sample());
+				}
+			}
+			_ => panic!("Unsupported channels found in input audio")
 		}
 	}
 }
